5c7382ee98902eba50e47422dffb59fd050c562a
Sergey Iryupin <biblelamp@yandex.ru>
2019-05-25 11:56:41 +0200

    added a bit

--- LightComp/different_queries.sql
+++ LightComp/different_queries.sql
@@ -38,15 +38,48 @@ WHERE rs.archive_id <> f.archive_id
 
 -- U archivních souborů/fondů z archivu XY přidat tématickou skupiny Z pokud fond má tématickou skupinu V
 
+SET @XY = 1;
+SET @Z = "010000";
+SET @V = "030100";
 
+-- Ktere zaznamy?
+-- hledame fund_id
+-- where archive_id = 'Y' tg.code='V'
+
+INSERT INTO fund_thema(fund_id, thematic_group_id)
+
+SELECT f.fund_id, t.thematic_group_id
+FROM fund f
+	JOIN fund_thema ft ON ft.fund_id = f.fund_id
+	JOIN thematic_group tg ON ft.thematic_group_id = tg.thematic_group_id
+	JOIN (SELECT tg.thematic_group_id FROM thematic_group tg WHERE tg.CODE = @Z) t
+WHERE f.archive_id = @XY AND tg.CODE = @V
+
+-- méně optimální možnost
+
+SELECT f.fund_id, (SELECT tg.thematic_group_id FROM thematic_group tg WHERE tg.CODE = @Z)
+FROM fund f
+	JOIN fund_thema ft ON ft.fund_id = f.fund_id
+	JOIN thematic_group tg ON ft.thematic_group_id = tg.thematic_group_id
+WHERE f.archive_id = @XY AND tg.code = @V
 
 -- Výpis archivních souborů (fund) s počtem archivních pomůcek (finding aid) a počtem badatelských listů (research sheet)
 
-SELECT f.fund_id, f.NAME, 
-	(SELECT COUNT(fa.finding_aid_id) FROM finding_aid fa 
-		WHERE fa.fund_id = f.fund_id) AS faCount,
-	(SELECT COUNT(rs.research_sheet_id) FROM research_sheet rs
-		JOIN accessible_fund af ON rs.research_sheet_id = af.research_sheet_id
-		WHERE f.fund_id = af.fund_id) AS rsCount
-FROM fund f
+SELECT f.fund_id, f.name, t1.count, t2.count FROM fund f
+	LEFT JOIN (
+		SELECT fa.fund_id, COUNT(fa.finding_aid_id) AS count
+		FROM finding_aid fa
+		GROUP BY fa.fund_id) t1
+	ON f.fund_id = t1.fund_id
+	LEFT JOIN (
+		SELECT af.fund_id, COUNT(af.accessible_fund_id) AS count
+		FROM accessible_fund af
+		GROUP BY af.fund_id) t2
+	ON f.fund_id = t2.fund_id
 
+-- SELECT fa.fund_id, COUNT(fa.finding_aid_id) FROM finding_aid fa
+-- GROUP BY fa.fund_id
+
+-- SELECT f.fund_id AS id, COUNT(fa.fund_id) as count FROM fund f
+-- 	LEFT JOIN finding_aid fa ON f.fund_id = fa.fund_id
+-- GROUP BY f.fund_id, f.NAME
