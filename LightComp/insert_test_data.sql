858c333d87c9c8f2281d0297bb4abd762e48b5d5
Sergey Iryupin <biblelamp@yandex.ru>
2019-05-28 21:13:29 +0200

    added a bit

--- LightComp/insert_test_data.sql
+++ LightComp/insert_test_data.sql
@@ -55,8 +55,8 @@ VALUES
 INSERT INTO research_sheet
 	(valid_to, archive_id, rstype, scope, legal_person, rsstate, approval_date, user_id)
 VALUES
-	(null, 1, "rstype", null, null, "rsstate", null, 1),
-	(null, 1, "rstype", null, null, "rsstate", null, 1),
+	(null, 1, "rstype", "výzkumu", null, "rsstate", null, 1),
+	(null, 1, "rstype", "výzkumu", null, "rsstate", null, 1),
 	(NULL, 2, "rstype", null, null, "rsstate", null, 1),
 	(NULL, 2, "rstype", null, null, "rsstate", null, 1),
 	(NULL, 2, "rstype", null, null, "rsstate", null, 1),
