767437e215136bd24b8bbc0d82574898cea69af2
Sergey Iryupin <biblelamp@yandex.ru>
2019-07-12 11:08:41 +0200

    added and changed

--- LightComp/insert_test_data.sql
+++ LightComp/insert_test_data.sql
@@ -82,32 +82,32 @@ INSERT INTO downloadable_file
 VALUES
 	(1, "text/plain", "2019-07-11", "filename.txt", 1024, "file");
 
--- DELETE FROM `event`
-INSERT INTO `event`
-	(queue_name, category_name, state, inserted, order_id)
-VALUES
-	("delete", "1", "PRIPRAVENA", "2019-07-10", 1);
-
--- DELETE FROM dao;
-INSERT INTO dao
-	(information_package_id, filename, mimetype, size, position)
-VALUES
-	(1, "filename.txt", "plain/text", 1024, 1);
-
 -- DELETE FROM information_package;
 INSERT INTO information_package
 	(information_package_id, order_id, uuid, path, size)
 VALUES
 	(1, 1, "1234567890abcdef", "path/to", 1024);
 
--- DELETE FROM level
-INSERT INTO `level`
-	(order_id, information_package_id, description, position, depth, leaf)
-VALUE
-	(1, 1, "description", 1, 1, 1);
-
 -- DELETE FROM level_view
 INSERT INTO level_view
-	(order_id, depth, leaf, hidden, description, position, type)
+	(level_view_id, order_id, information_package_id, depth, leaf, hidden, description, position, type)
 VALUE
-	(1, 1, 1, 0, "description", 1, "type");
+	(1, 1, 1, 1, 1, 0, "description", 1, "type");
+
+-- DELETE FROM level
+INSERT INTO `level`
+	(level_id, order_id, level_view_id, information_package_id, description, position, depth, leaf)
+VALUE
+	(1, 1, 1, 1, "description", 1, 1, 1);
+
+-- DELETE FROM dao;
+INSERT INTO dao
+	(dao_id, information_package_id, level_id, level_view_id, filename, mimetype, size, position)
+VALUES
+	(1, 1, 1, 1, "filename.txt", "plain/text", 1024, 1);
+
+-- DELETE FROM `event`
+INSERT INTO `event`
+	(queue_name, category_name, state, inserted, order_id)
+VALUES
+	("delete", "1", "PRIPRAVENA", "2019-07-10", 1);
