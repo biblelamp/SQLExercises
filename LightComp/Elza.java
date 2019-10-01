17e4f914891435623bd6d764b44e7c454e9fb3fe
Sergey Iryupin <biblelamp@yandex.ru>
2019-10-01 12:36:09 +0200

    added for eliza

---
+++ LightComp/Elza.java
@@ -0,0 +1,7 @@
+class Elza {
+    public static void main(String[] args) {
+        for (int i = 2; i < 250; i++) {
+            System.out.println("\t("+ i +", CURRENT_TIMESTAMP, 'test" + i + "', 88),");
+        }
+    }
+}
---
+++ LightComp/eliza.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.arr_fund
+	(fund_id, create_date, name, institution_id)
+VALUES
+	(2, CURRENT_TIMESTAMP, 'test2', 88);
+
+SELECT * FROM public.arr_fund;
