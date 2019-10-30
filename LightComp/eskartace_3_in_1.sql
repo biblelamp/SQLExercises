1d8ec6add0624e2b37c8fd9e365d6b10abaa61bd
Sergey Iryupin <biblelamp@yandex.ru>
2019-10-30 09:31:38 +0100

    fixed query a bit

--- LightComp/eskartace_3_in_1.sql
+++ LightComp/eskartace_3_in_1.sql
@@ -1,7 +1,7 @@
 SELECT lv.zobrazeni_urovne_id, lv.typ, ld.popis, l.skartacni_znak, l.skartacni_lhuta, l.identifikator_puvodce
 FROM zobrazeni_urovne lv
 	JOIN popis_urovne ld ON lv.popis_urovne_id = ld.popis_urovne_id
-	JOIN uroven_popisu l ON l.zobrazeni_urovne_id = lv.zobrazeni_urovne_id
-WHERE lv.rizeni_id = 1 
-	AND lv.zobrazeni_urovne_id = 81
-	AND l.uroven_popisu_id = (SELECT MIN(uroven_popisu_id) FROM uroven_popisu WHERE zobrazeni_urovne_id = 81)
+	LEFT JOIN uroven_popisu l ON l.zobrazeni_urovne_id = lv.zobrazeni_urovne_id
+WHERE lv.rizeni_id = 2 
+	AND lv.zobrazeni_urovne_id = 5
+	AND (l.uroven_popisu_id = (SELECT MIN(uroven_popisu_id) FROM uroven_popisu WHERE zobrazeni_urovne_id = 5) OR l.uroven_popisu_id IS NULL)
