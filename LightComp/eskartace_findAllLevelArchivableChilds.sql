84bce542c17d986e8f0154e5dc649941689e1158
Sergey Iryupin <biblelamp@yandex.ru>
2019-11-04 17:02:57 +0100

    refactored

--- LightComp/eskartace_findAllLevelArchivableChilds.sql
+++ LightComp/eskartace_findAllLevelArchivableChilds.sql
@@ -1,23 +1,23 @@
 SELECT zobrazeni_urovne_id, rodic_id, list, computed_depth, typ, nazev_urovne, skartacni_znak, zamek, rozhodnuti
 FROM (WITH RECURSIVE cte 
-	(zobrazeni_urovne_id, rodic_id, poradi, expanded, sort_col, computed_depth, typ, list, nazev_urovne, skartacni_znak, zamek, rozhodnuti)
-	AS (SELECT zu.zobrazeni_urovne_id, zu.rodic_id, zu.poradi, zu.zobrazeni_urovne_id IN (9223372036854775807)  AS expanded,
-			CAST(LPAD(poradi,6,0) as char(128)) AS sort_col,
+		(zobrazeni_urovne_id, rodic_id, poradi, expanded, sort_col, computed_depth, typ, list, nazev_urovne, skartacni_znak, zamek, rozhodnuti)
+	AS (SELECT zu.zobrazeni_urovne_id, zu.rodic_id, zu.poradi, zu.zobrazeni_urovne_id IN (9223372036854775807) AS expanded,
+			CAST(LPAD(poradi, 6, 0) AS CHAR(128)) AS sort_col,
          IF(zu.typ IN ('SPIS', 'DOKUMENT', 'KOMPONENTA'), 1, 0) computed_depth,
-         zu.typ, zu.list, zu.nazev_urovne, zu.skartacni_znak, zu.zamek, zu.rozhodnuti
+         	zu.typ, zu.list, zu.nazev_urovne, zu.skartacni_znak, zu.zamek, zu.rozhodnuti
          FROM zobrazeni_urovne zu
-         WHERE zu.rodic_id = 2 and zu.rizeni_id = 1
-		UNION distinct
+         WHERE zu.rodic_id = 6 AND zu.rizeni_id = 1
+		UNION DISTINCT
 			SELECT zu.zobrazeni_urovne_id, zu.rodic_id, zu.poradi, zu.zobrazeni_urovne_id IN (9223372036854775807),
 				CONCAT(cte.sort_col, LPAD(zu.poradi, 6, '0')),
             IF(zu.typ IN ('SPIS', 'DOKUMENT', 'KOMPONENTA'), cte.computed_depth + 1, 0) computed_depth,
-				zu.typ, zu.list, zu.nazev_urovne, zu.skartacni_znak, zu.zamek, zu.rozhodnuti
-            FROM cte
-       JOIN zobrazeni_urovne zu on zu.rodic_id = cte.zobrazeni_urovne_id
+					zu.typ, zu.list, zu.nazev_urovne, zu.skartacni_znak, zu.zamek, zu.rozhodnuti
+         FROM cte
+       	JOIN zobrazeni_urovne zu ON zu.rodic_id = cte.zobrazeni_urovne_id
 				WHERE (cte.computed_depth = 0 OR cte.expanded = TRUE) AND zu.typ IN ('SPIS', 'DOKUMENT', 'KOMPONENTA') AND zu.rizeni_id = 1
        )
 	SELECT zobrazeni_urovne_id, rodic_id, list, computed_depth, typ, sort_col, nazev_urovne, skartacni_znak, zamek, rozhodnuti
 	FROM cte
 ) x
 	WHERE x.computed_depth > 0
-   ORDER by sort_col
+   ORDER BY sort_col
