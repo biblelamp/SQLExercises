dcc7179e7c46895478869eb200c877d0349268df
Sergey Iryupin <biblelamp@yandex.ru>
2019-12-09 11:15:27 +0100

    changed a bit

--- LightComp/ezpristupneni_recurive_level_view_full.sql
+++ LightComp/ezpristupneni_recurive_level_view_full.sql
@@ -4,7 +4,7 @@ WITH RECURSIVE cte (uuid, filename, level_view_id, parent_id, description) AS
 	  JOIN cart_item c ON c.level_view_id=l.level_view_id
 	  JOIN information_package i ON i.information_package_id=l.information_package_id
 	  JOIN dao d ON d.level_view_id=l.level_view_id
-  	WHERE c.cart_item_id = 1
+  	WHERE c.cart_order_id = 6
 	UNION DISTINCT
 	SELECT  cte.uuid, cte.filename, lv.level_view_id, lv.parent_id, lv.description FROM cte
 	  JOIN level_view lv ON cte.level_view_id=lv.parent_id
