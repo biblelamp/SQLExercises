5b80239f90d9d74c2bc8c45e7811550f95ef373f
Sergey Iryupin <biblelamp@yandex.ru>
2019-12-09 11:05:00 +0100

    recursive cte level_view full

---
+++ LightComp/ezpristupneni_recurive_level_view_full.sql
@@ -0,0 +1,12 @@
+WITH RECURSIVE cte (uuid, filename, level_view_id, parent_id, description) AS
+(
+	SELECT i.uuid, d.filename, l.level_view_id, l.parent_id, l.description FROM level_view l
+	  JOIN cart_item c ON c.level_view_id=l.level_view_id
+	  JOIN information_package i ON i.information_package_id=l.information_package_id
+	  JOIN dao d ON d.level_view_id=l.level_view_id
+  	WHERE c.cart_item_id = 1
+	UNION DISTINCT
+	SELECT  cte.uuid, cte.filename, lv.level_view_id, lv.parent_id, lv.description FROM cte
+	  JOIN level_view lv ON cte.level_view_id=lv.parent_id
+)
+SELECT * FROM cte
