2953c4cea2950178e21d9edccb8485dc6cdbde94
Sergey Iryupin <biblelamp@yandex.ru>
2019-12-10 08:00:05 +0100

    fixed a bit

--- LightComp/ezpristupneni_recurive_level_view_full.sql
+++ LightComp/ezpristupneni_recurive_level_view_full.sql
@@ -3,10 +3,12 @@ WITH RECURSIVE cte (uuid, filename, level_view_id, parent_id, description) AS
 	SELECT i.uuid, d.filename, l.level_view_id, l.parent_id, l.description FROM level_view l
 	  JOIN cart_item c ON c.level_view_id=l.level_view_id
 	  JOIN information_package i ON i.information_package_id=l.information_package_id
-	  JOIN dao d ON d.level_view_id=l.level_view_id
-  	WHERE c.cart_order_id = 6
+	  LEFT JOIN dao d ON d.level_view_id=l.level_view_id
+  	WHERE c.cart_order_id = 8
 	UNION DISTINCT
-	SELECT  cte.uuid, cte.filename, lv.level_view_id, lv.parent_id, lv.description FROM cte
+	SELECT  cte.uuid, dao.filename, lv.level_view_id, lv.parent_id, lv.description FROM cte
 	  JOIN level_view lv ON cte.level_view_id=lv.parent_id
+	  LEFT JOIN dao dao ON dao.level_view_id=lv.level_view_id
 )
 SELECT * FROM cte
+WHERE filename IS NOT null
