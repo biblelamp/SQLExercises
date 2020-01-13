2b8053dd4dfc5cf4a1682dc3759ed8a1449d62e1
Sergey Iryupin <biblelamp@yandex.ru>
2020-01-13 13:49:51 +0100

    added test data

--- LightComp/eprejimka_sql.sql
+++ LightComp/eprejimka_sql.sql
@@ -1,3 +1,9 @@
+-- DELETE FROM event;
+INSERT INTO `event`
+	(queue_name, category_name, state, inserted, prejimka_id)
+VALUES
+	("prepare_editor", "create", "PRIPRAVENA", CURRENT_TIMESTAMP, 1);
+
 SELECT *
 FROM informacni_balicek ib
 WHERE ib.informacni_balicek_id = (SELECT MIN(ib.informacni_balicek_id) FROM informacni_balicek ib WHERE ib.prejimka_id=2);
