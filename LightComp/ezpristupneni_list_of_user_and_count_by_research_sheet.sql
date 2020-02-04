745ecf8a342b672364d0145106ba8a18c0c6df73
Sergey Iryupin <biblelamp@yandex.ru>
2020-02-04 16:09:47 +0100

    added filtrace

--- LightComp/ezpristupneni_list_of_user_and_count_by_research_sheet.sql
+++ LightComp/ezpristupneni_list_of_user_and_count_by_research_sheet.sql
@@ -1,3 +1,4 @@
 SELECT u.user_id, u.user, u.first_name, u.last_name, u.verified, COUNT(rs.research_sheet_id) FROM user u
 LEFT JOIN research_sheet rs ON rs.user_id=u.user_id AND rs.rsstate='NEW'
+WHERE u.user LIKE '%serg%' OR u.first_name LIKE '%serg%' OR u.last_name LIKE '%serg%'
 GROUP BY u.user_id
