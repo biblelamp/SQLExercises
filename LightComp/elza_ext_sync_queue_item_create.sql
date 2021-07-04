978063370a8fa0298be2363678283ddb442dd3ae
Sergey Iryupin <biblelamp@yandex.ru>
2021-07-04 08:03:23 +0200

    insert current date and time

---
+++ LightComp/elza_ext_sync_queue_item_create.sql
@@ -0,0 +1,6 @@
+INSERT INTO ext_syncs_queue_item
+	(ext_syncs_queue_item_id, access_point_id, external_system_id, state_message, state, date, username)
+VALUES
+	(1, 2, 24, 'abcd1', 'OK', CURRENT_TIMESTAMP, 'admin'),
+	(2, 2, 24, 'abcd2', 'OK', CURRENT_TIMESTAMP, 'admin'),
+	(3, 2, 24, 'abcd3', 'OK', CURRENT_TIMESTAMP, 'admin')
