7c7dfdca64ab2f45c06e0881f051392140cd8d46
Sergey Iryupin <biblelamp@yandex.ru>
2021-12-19 18:53:58 +0100

    check coordinates

---
+++ LightComp/elza_arr_data_ coodrinates_check.sql
@@ -0,0 +1,8 @@
+select * from arr_fund
+
+select * from arr_node where fund_id = 1
+select * from arr_level where node_id in (1,2,3,4)
+
+select * from rul_item_type where code = 'ZP2015_POSITION'
+select * from arr_data where data_type_id = 7
+select * from arr_data_coordinates where data_id in (1072,1073)
