812d60af9da65320bba39d398ddfcd3c785e1fec
Sergey Iryupin <biblelamp@yandex.ru>
2022-06-05 16:17:03 +0200

    bug 7655

---
+++ LightComp/elza_packages_7655.sql
@@ -0,0 +1,13 @@
+select * from rul_item_spec where item_spec_id=925
+select * from rul_item_type where code='IDN_TYPE'
+select * from rul_item_type 
+select * from rul_item_type_spec_assign where item_type_id=7 order by item_spec_view_order
+select * from rul_package_dependency
+select * from rul_package
+
+select * from rul_arrangement_extension where code like '%PERSON_INDIVIDUAL/PT_IDENT%'
+select * from rul_extension_rule where component_id in (109,182)
+select * from rul_component where component_id=182
+
+select * from rul_rule_set
+select * from rul_set
