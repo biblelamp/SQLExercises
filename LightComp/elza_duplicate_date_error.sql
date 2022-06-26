7496db9ea0642a97076c26a368b34b413476eb5e
Sergey Iryupin <biblelamp@yandex.ru>
2022-06-26 14:56:07 +0200

    duplicate date error

---
+++ LightComp/elza_duplicate_date_error.sql
@@ -0,0 +1,4 @@
+select * from arr_data_unitdate
+select * from ap_item where data_id in (1171)
+select * from ap_cached_access_point where access_point_id=246
+select * from ap_index where part_id=468
